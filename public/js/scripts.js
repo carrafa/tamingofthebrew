ac124cca6b1b7a347e0ffd35b66f216d82497c94
Joshua Carrafa <carrafa@gmail.com>
2016-04-16 17:00:29 -0400

    adds compare beer logic

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,30 @@
 console.log('to beer or not to beer');
 
+var testTasteMap = {
+  crisp: 10,
+  hop: 8,
+  nut: 1,
+  fruit: 3,
+  cream: 5,
+  dry: 5, 
+  sweet: 4,
+  bitter: 1,
+  spicy: 8,
+  sour: 7
+}
 
+function testCompare(){
+  $.ajax({
+    method: 'get',
+    url: '/api/beers/compare', 
+    data: testTasteMap,
+    success: function(response){
+      console.log(response);
+    }
+  });
+}
+
+
+$('input').on('input', function(){
+// stuff in here
+});
--- routes/api/beers.js
+++ routes/api/beers.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var Beer = require('../../models/beer.js');
 var beerCompare = require('../../lib/beerCompare');
 
-
 router.get('/', function(req, res){
   Beer.find({}, function(err, dbBeers){
     res.json({
@@ -12,12 +11,13 @@ router.get('/', function(req, res){
   });
 });
 
-
-router.post('/', function(req, res){
-  var matches = beerCompare(req.body.data);
-  res.json({
-    beers: matches
-  });
+router.get('/compare', function(req, res){
+  var tasteMap = req.query;
+  beerCompare(tasteMap, function(matches){
+   res.json({
+     beers:  matches
+    });
+  })
 });
 
 module.exports = router
