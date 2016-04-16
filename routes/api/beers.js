05f5af8735f19c36b5b021513d5b490bed87239f
Joshua Carrafa <carrafa@gmail.com>
2016-04-16 16:05:06 -0400

    starts beerCompare

--- routes/api/beers.js
+++ routes/api/beers.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var Beer = require('../../models/beer.js');
+var beerCompare = require('../../lib/beerCompare');
+
 
 router.get('/', function(req, res){
   Beer.find({}, function(err, dbBeers){
@@ -12,7 +14,10 @@ router.get('/', function(req, res){
 
 
 router.post('/', function(req, res){
-
+  var matches = beerCompare(req.body.data);
+  res.json({
+    beers: matches
+  });
 });
 
 module.exports = router
