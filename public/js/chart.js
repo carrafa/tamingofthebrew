58921ba180fe90b71096bb3f82afc8e9f9344648
Kyle Stengline <kyle.stengline@gmail.com>
2016-04-16 17:01:00 -0400

    adds lablels and sliders

--- public/js/chart.js
+++ public/js/chart.js
@@ -19,11 +19,11 @@ function selectElement(evt) {
 
 }
   selectedElement.setAttributeNS(null, "onmousemove", "moveElement(evt)");
-  }
+}
 
 var data = [
         {
-          className: 'flavors', // optional can be used for styling
+          className: 'flavors',
           axes: [
             {axis: "crisp", value: 3},
             {axis: "hop", value: 3},
@@ -59,6 +59,59 @@ var svg = d3.select('body').append('svg')
 .attr('width', cfg.w + cfg.w + 50)
 .attr('height', cfg.h + cfg.h / 4);
 svg.append('g').classed('single', 1).datum(randomDataset()).call(chart);
+
+
+
+var chart = RadarChart.chart();
+var svg = d3.select('body').append('svg')
+  .attr('width', 600)
+  .attr('height', 800);
+
+// draw one
+svg.append('g').classed('focus', 1).datum(data).call(chart);
+
+// draw many radars
+var game = svg.selectAll('g.game').data(
+  [
+    data,
+    data,
+    data,
+    data
+  ]
+);
+game.enter().append('g').classed('game', 1);
+game
+  .attr('transform', function(d, i) { return 'translate(150,600)'; })
+  .call(chart);
+
+  chart.config();
+// all options with default values
+chart.config({
+  containerClass: 'radar-chart', // target with css, default stylesheet targets .radar-chart
+  w: 600,
+  h: 600,
+  factor: 0.95,
+  factorLegend: 1,
+  levels: 3,
+  maxValue: 0,
+  minValue: 0,
+  radians: 2 * Math.PI,
+  color: d3.scale.category10(), // pass a noop (function() {}) to decide color via css
+  axisLine: true,
+  axisText: true,
+  circles: true,
+  radius: 5,
+  axisJoin: function(d, i) {
+    return d.className || i;
+  },
+  tooltipFormatValue: function(d) {
+    return d;
+  },
+  tooltipFormatClass: function(d) {
+    return d;
+  },
+  transitionDuration: 300
+});
 
 render();
 
