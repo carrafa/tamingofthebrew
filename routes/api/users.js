d7c85df4afbdc79e895fba82cff29788b139a374
Joshua Carrafa <carrafa@gmail.com>
2016-04-17 17:03:06 -0400

    cleanup

--- routes/api/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var User = require('../../models/user.js');
-
-router.get('/', function(req, res){
-});
-
-module.exports = router;
