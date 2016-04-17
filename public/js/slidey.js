d6d7e3584f69bd2692e3b9a41328664fd544111d
ruby -v ruby -v <ajhofferber@gmail.com>
2016-04-17 12:04:28 -0400

    changes legend font

--- public/js/slidey.js
+++ public/js/slidey.js
@@ -130,7 +130,7 @@ var RadarChartSlidey = {
           .attr("class", "line").style("stroke", "grey").style("stroke-width", "1px");
 
       axis.append("text").attr("class", "legend")
-          .text(function(d){return d}).style("font-family", "sans-serif").style("font-size", "18px").attr("transform", function(d, i){return "translate(0, -10)";})
+          .text(function(d){return d}).style("font-family", "futura").style("font-size", "18px").attr("transform", function(d, i){return "translate(0, -10)";})
           .attr("x", function(d, i){return cfg.w/2*(1-cfg.factorLegend*Math.sin(i*cfg.radians/total))-20*Math.sin(i*cfg.radians/total);})
           .attr("y", function(d, i){return cfg.h/2*(1-Math.cos(i*cfg.radians/total))+20*Math.cos(i*cfg.radians/total);});
     }
@@ -217,7 +217,7 @@ var RadarChartSlidey = {
 
 
     //Tooltip
-    tooltip = g.append('text').style('opacity', 0).style('font-family', 'sans-serif').style('font-size', 13);
+    tooltip = g.append('text').style('opacity', 0).style('font-family', 'futura').style('font-size', 13);
 
 
 
